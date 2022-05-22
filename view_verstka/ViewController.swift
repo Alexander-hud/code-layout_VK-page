a6d8981ffed0ef31d567c74ddfd8b406c8ad70a5
Mogan <krsk.1994@mail.ru>
2022-05-22 17:08:23 +0400

    Initial Commit

---
+++ view_verstka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  view_verstka
+//
+//  Created by Александр Алешин on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
