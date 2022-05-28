753d0833bc033877e843c16419a838315d288375
Mogan <krsk.1994@mail.ru>
2022-05-28 17:11:21 +0400

    Create method buttons in tabbar.

--- view_verstka/ViewController.swift
+++ view_verstka/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController  {
         return stackView
     }()
     
+    private lazy var tabbatStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 40
+        stackView.backgroundColor = .red
+        return stackView
+    }()
+    
     private lazy var imageProfile: UIImageView = {
         var image = UIImageView(frame: CGRect(x: Metric.uIImageViewX, y: Metric.uIImageViewY, width: Metric.uIImageViewWidth, height: Metric.uIImageViewHeight))
         image.image = UIImage(named: Strings.nameImage)
@@ -54,6 +62,10 @@ class ViewController: UIViewController  {
     }()
     
     private lazy var buttonEdit = createButton(with: Strings.textButton, titleColor: .white, background: .darkGray)
+    private lazy var buddin1 = createButton(with: "История", titleColor: .red, background: .blue)
+    private lazy var buddin2 = createButton(with: "Запись", titleColor: .red, background: .blue)
+    private lazy var buddin3 = createButton(with: "Фото", titleColor: .red, background: .blue)
+    private lazy var buddin4 = createButton(with: "Клип", titleColor: .red, background: .blue)
   
 
 // MARK: - Lifecycle
@@ -62,6 +74,7 @@ class ViewController: UIViewController  {
         setupView()
         setupHierarchy()
         setupLayout()
+        buttonEdit.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
 
 // MARK: - Settings
@@ -72,13 +85,27 @@ class ViewController: UIViewController  {
         perentStackView.addArrangedSubview(labelStatus)
         perentStackView.addArrangedSubview(labelStatusNetwork)
         view.addSubview(buttonEdit)
+        view.addSubview(tabbatStackView)
+        tabbatStackView.addArrangedSubview(buddin1)
+        tabbatStackView.addArrangedSubview(buddin2)
+        tabbatStackView.addArrangedSubview(buddin3)
+        tabbatStackView.addArrangedSubview(buddin4)
     }
 
     private func setupView() {
        view.backgroundColor = .black
     }
     
-    
+//    private func createButtonTabBar(with title: String, titleColor: UIColor, image: UIImageView) -> UIButton{
+//        let button = UIButton(type: .system)
+//        button.setTitle(title, for: .normal)
+//        button.setTitleColor(titleColor, for: .normal)
+//        button.titleLabel?.font = .systemFont(ofSize: Metric.buttonTextSize, weight: .medium)
+//        button.image = image
+//        button.layer.cornerRadius = Metric.buttonRadius
+//
+//        return button
+//    }
     
     private func createButton(with title: String, titleColor: UIColor, background: UIColor) -> UIButton {
         let button = UIButton(type: .system)
@@ -91,15 +118,37 @@ class ViewController: UIViewController  {
         return button
     }
     
+    // MARK: Create TABBARCONTROLLER
+    @objc func didTapButton() {
+        let tabBarVC = UITabBarController()
+        
+        let vc1 = FirstViewController()
+        let vc2 = SecondViewController()
+        let vc3 = ThirdViewController()
+        let vc4 = FirstViewController()
+        tabBarVC.setViewControllers([vc1, vc2, vc3, vc4], animated: false)
+        
+        tabBarVC.modalPresentationStyle = .fullScreen
+        present(tabBarVC, animated: true)
+    }
+    
+    
     private func setupLayout() {
         imageProfile.translatesAutoresizingMaskIntoConstraints = true
         imageProfile.bottomAnchor.constraint(equalTo: perentStackView.bottomAnchor, constant: 0).isActive = true
         
         perentStackView.translatesAutoresizingMaskIntoConstraints = false
         perentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Metric.pSVLeadingAnchor).isActive = true
-        perentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -Metric.pSVtrailingAnchor).isActive = true
+        perentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Metric.pSVtrailingAnchor).isActive = true
         perentStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Metric.pSVtopAnchor).isActive = true
         perentStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: Metric.buttonRightAnchor).isActive = true
+        
+        tabbatStackView.translatesAutoresizingMaskIntoConstraints = false
+        tabbatStackView.widthAnchor.constraint(equalToConstant: .greatestFiniteMagnitude).isActive = true
+        tabbatStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+        tabbatStackView.topAnchor.constraint(equalTo: buttonEdit.bottomAnchor, constant: Metric.buttonTopAnchor).isActive = true
+        tabbatStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Metric.buttonLeftAnchor).isActive = true
+        tabbatStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: Metric.buttonRightAnchor).isActive = true
         
         labelFullName.topAnchor.constraint(equalTo: perentStackView.topAnchor, constant: Metric.labelTopAnchor).isActive = true
         labelFullName.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Metric.labelTopAnchor).isActive = true
@@ -120,6 +169,7 @@ class ViewController: UIViewController  {
         buttonEdit.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Metric.buttonLeftAnchor).isActive = true
         buttonEdit.rightAnchor.constraint(equalTo: view.rightAnchor, constant: Metric.buttonRightAnchor).isActive = true
         
+      
     }
 
 }
@@ -183,3 +233,33 @@ extension UIColor {
        )
    }
 }
+
+class FirstViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+}
+class SecondViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+
+    }
+}
+class ThirdViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+class FourthViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+
+    }
+}
+
+
